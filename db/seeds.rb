941564e4cf9093b416e71aca206f9fecea842c4e
Jonathan McLachlan <jonathan.mclachlan@hotmail.com>
2022-05-24 17:39:37 +0200

    cloudinary not working but otherwise fine second commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,18 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# major_floats_owner = User.new(name: "Jon", email: "jonathan.mclachlan@hotmail.com", password: "123456")
+
+major_floats_owner = User.create(name: "Jon", email: "jonathan.mclachlan@hotmail.com", password: "123456")
 
 require "open-uri"
 
-major_floats_owner = User.find("1")
+# major_floats_owner = User.find("1")
 
 Floatdevice.destroy_all
 
 unicorn = Floatdevice.new(name: "Unicorn", price: 5, category: "Animal", size: 1, description: "Really nice floaty unicorn")
-unicorn_pic = URI.open('https://res.cloudinary.com/dg9murjje/image/upload/v1653403129/unicorn-float_nd3fsp.jpg')
-unicorn.photo.attach(io: unicorn_pic, filename: 'unicorn.png', content_type: 'image/png')
+unicorn_pic = URI.open('https://img.fruugo.com/product/4/94/193638944_max.jpg')
+unicorn.photo.attach(io: unicorn_pic, filename: 'unicorn.jpg', content_type: 'image/jpg')
 unicorn.user = major_floats_owner
 unicorn.save
 
