548c2edf7a6e612cfb0df8de5de1efa9b42f5502
Eden Lan <lanailin4@gmail.com>
2022-05-24 14:41:18 +0200

    finish the models-validation and tests

--- app/models/floatdevice.rb
+++ app/models/floatdevice.rb
@@ -1,3 +1,9 @@
 class Floatdevice < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :category, presence: true
+  validates :size, presence: true
+  validates :description, presence: true
 end
