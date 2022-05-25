ca0f558462ab0d478887eec9d34c2f7610462295
Jonathan McLachlan <jonathan.mclachlan@hotmail.com>
2022-05-25 12:10:54 +0200

    updated controller

--- app/controllers/floatdevices_controller.rb
+++ app/controllers/floatdevices_controller.rb
@@ -8,9 +8,23 @@ class FloatdevicesController < ApplicationController
   def show
   end
 
+  def new
+    @floatdevice = Floatdevice.new
+  end
+
+  def create
+    @floatdevice = Floatdevice.new(floatdevice_params)
+    @floatdevice.user = User.find("1")
+    @floatdevice.save
+  end
+
   private
 
   def set_floatdevice
     @floatdevice = Floatdevice.find(params[:id])
+  end
+
+  def floatdevice_params
+    params.require(:floatdevice).permit(:name, :price, :category, :size, :description, :special_features, :photo)
   end
 end
