f5c3aa6520060ae6d59f8840836aff1f76cabb4f
ConnorFord <fordc9191@gmail.com>
2022-05-25 15:01:27 +0200

    New booking form

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,26 @@
+class BookingsController < ApplicationController
+
+  def new
+    @booking = Booking.new
+    @floatdevice = Floatdevice.find(params[:floatdevice_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @floatdevice = Floatdevice.find(params[:floatdevice_id])
+    @booking.floatdevice = @floatdevice
+    if @booking.save
+      redirect_to floatdevice_path(@floatdevice)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
+  end
+
+end
